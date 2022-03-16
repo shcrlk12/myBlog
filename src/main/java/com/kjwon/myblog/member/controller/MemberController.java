13d406630e0743c69be132a7f870c40a03f4d16c
JeongWon <shcrlk12@naver.com>
2022-03-16 20:18:57 +0900

    second commit

--- src/main/java/com/kjwon/myblog/member/controller/MemberController.java
+++ src/main/java/com/kjwon/myblog/member/controller/MemberController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
