66cad58f77fa3cbd3dd9670dcec8a36c2afefd1d
JeongWon <shcrlk12@naver.com>
2022-03-17 15:46:32 +0900

    리팩토링중

--- src/main/java/com/kjwon/myblog/member/controller/MemberController.java
+++ src/main/java/com/kjwon/myblog/member/controller/MemberController.java
@@ -3,8 +3,12 @@ package com.kjwon.myblog.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 
 @RequiredArgsConstructor
@@ -32,11 +36,11 @@ public class MemberController {
 //        return "member/find_password_result";
 //    }
 //
-//    @GetMapping("/member/register")
-//    public String register() {
-//
-//        return "member/register";
-//    }
+    @GetMapping("/member/register")
+    public String register() {
+
+        return "member/register";
+    }
 //
 //    @PostMapping("/member/register")
 //    public String registerSubmit(Model model, HttpServletRequest request
@@ -55,28 +59,28 @@ public class MemberController {
 //    // https://www.naver.com/cafe/detail.do?id=2222
 //
 //
-//    @GetMapping("/member/email-auth")
-//    public String emailAuth(Model model, HttpServletRequest request) {
-//
-//        String uuid = request.getParameter("id");
-//        System.out.println(uuid);
-//
+    @GetMapping("/member/email-auth")
+    public String emailAuth(Model model, HttpServletRequest request) {
+
+        String uuid = request.getParameter("id");
+        System.out.println(uuid);
+
 //        boolean result = memberService.emailAuth(uuid);
 //        model.addAttribute("result", result);
-//
-//        return "member/email_auth";
-//    }
-//
-//    @GetMapping("/member/info")
-//    public String memberInfo(Model model, Principal principal) {
-//
+
+        return "member/email_auth";
+    }
+
+    @GetMapping("/member/info")
+    public String memberInfo(Model model, Principal principal) {
+
 //        String userId = principal.getName();
 //        MemberDto detail = memberService.detail(userId);
 //
 //        model.addAttribute("detail", detail);
-//
-//        return "member/info";
-//    }
+
+        return "member/info";
+    }
 //
 //    @PostMapping("/member/info")
 //    public String memberInfoSubmit(Model model
@@ -94,16 +98,16 @@ public class MemberController {
 //        return "redirect:/member/info";
 //    }
 //
-//    @GetMapping("/member/password")
-//    public String memberPassword(Model model, Principal principal) {
-//
-//        String userId = principal.getName();
+    @GetMapping("/member/password")
+    public String memberPassword(Model model, Principal principal) {
+
+        String userId = principal.getName();
 //        MemberDto detail = memberService.detail(userId);
 //
 //        model.addAttribute("detail", detail);
-//
-//        return "member/password";
-//    }
+
+        return "member/password";
+    }
 //
 //    @PostMapping("/member/password")
 //    public String memberPasswordSubmit(Model model
@@ -122,11 +126,11 @@ public class MemberController {
 //        return "redirect:/member/info";
 //    }
 //
-//    @GetMapping("/member/withdraw")
-//    public String memberWithdraw(Model model) {
-//
-//        return "member/withdraw";
-//    }
+    @GetMapping("/member/withdraw")
+    public String memberWithdraw(Model model) {
+
+        return "member/withdraw";
+    }
 //
 //    @PostMapping("/member/withdraw")
 //    public String memberWithdrawSubmit(Model model
@@ -144,16 +148,16 @@ public class MemberController {
 //        return "redirect:/member/logout";
 //    }
 //
-//    @GetMapping("/member/reset/password")
-//    public String resetPasswrod(Model model, HttpServletRequest request) {
-//        String uuid = request.getParameter("id");
-//
+    @GetMapping("/member/reset/password")
+    public String resetPasswrod(Model model, HttpServletRequest request) {
+        String uuid = request.getParameter("id");
+
 //        boolean result = memberService.checkResetPassword(uuid);
 //
 //        model.addAttribute("result", result);
-//
-//        return "member/reset_password";
-//    }
+
+        return "member/reset_password";
+    }
 //
 //    @PostMapping("/member/reset/password")
 //    public String resetPasswrodSubmit(Model model, ResetPasswordInput parameter) {
