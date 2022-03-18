b3e1b1902f32e4879099c882ee5846cb41fb2fbf
JeongWon <shcrlk12@naver.com>
2022-03-18 11:53:59 +0900

    시큐리티 테스트

--- src/main/java/com/kjwon/myblog/configuration/SecurityConfiguration.java
+++ src/main/java/com/kjwon/myblog/configuration/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         , "/member/email-auth"
                         , "/member/find-password"
                         , "/member/reset/password"
-                        , "/usr/local/tomcat/banners"
+                        , "/img/**"
                 )
                 .permitAll();
     
