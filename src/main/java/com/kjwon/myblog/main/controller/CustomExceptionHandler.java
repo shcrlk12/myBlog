f19fc1b3076e3b661a58d4d31bb3354ffc9972b7
JeongWon <shcrlk12@naver.com>
2022-03-18 15:37:21 +0900

    blog 글 쓰기 기능 보안

---
+++ src/main/java/com/kjwon/myblog/main/controller/CustomExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.kjwon.myblog.main.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    private  static final Logger log = Logger.getLogger(CustomExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public void Test(Exception e){
+        log.error(e.getStackTrace());
+    }
+}
