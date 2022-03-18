c66a3b034538b548bae8865d9f1466471f112591
JeongWon <shcrlk12@naver.com>
2022-03-18 12:59:43 +0900

    배너 테스트

--- src/main/java/com/kjwon/myblog/configuration/MvcConfiguration.java
+++ src/main/java/com/kjwon/myblog/configuration/MvcConfiguration.java
@@ -18,7 +18,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
         /* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60 * 60 * 24 * 365);
         /* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
-        registry.addResourceHandler("/img/**").addResourceLocations("file:/usr/local/tomcat/banners/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/img/**").addResourceLocations("file:/usr/local/tomcat/img/").setCachePeriod(60 * 60 * 24 * 365);
         /* '/font/**'로 호출하는 자원은 '/static/font/' 폴더 아래에서 찾는다. */
         registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60 * 60 * 24 * 365);
     }
